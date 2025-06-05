c7ea0a25d78fddcc136a0352e88e099a79948171
minh18ndh <minh.nguyendohoang@gmail.com>
2025-06-05 23:41:42 +0700

    add ngrok config

--- src/apis/apiFetch.js
+++ src/apis/apiFetch.js
@@ -4,7 +4,10 @@ export const STATIC_URL = BASE_URL.replace(/\/api$/, '');
 const getToken = () => localStorage.getItem('rentahouse_token');
 
 const apiFetch = async (endpoint, { method = 'GET', body, token = getToken() } = {}) => {
-    const headers = { 'Content-Type': 'application/json' };
+    const headers = {
+        'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': 'true',
+    };
     if (token) headers.Authorization = `Bearer ${token}`;
 
     const res = await fetch(`${BASE_URL}${endpoint}`, {
